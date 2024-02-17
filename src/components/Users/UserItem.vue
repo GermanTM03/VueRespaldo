403f83cfda468faa047038df4199321f9182f2bc
German Torres <german500tm@gmail.com>
2024-02-17 18:33:48 -0500

    Initial commit

---
+++ src/components/Users/UserItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <h2>#{{ props.id }} {{ name }} | {{ props.lastName }}</h2>
+    <h3>Email: <span>{{ props.email }}</span></h3>
+    <h3>Age: <span>{{ props.age }}</span></h3>
+    <h3>Email: <span>{{ props.email }}</span></h3>
+    <!-- <input type="text" v-model="name"> -->
+    <input type="text" v-model="change">
+    <h1>{{ changev }}</h1>
+    <button :disabled="validationchange" @click="clcikHandler()">Click me</button>
+    
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { IuserInfo } from '@/interfaces/IUserInfo'
+import { ref,computed } from 'vue';
+import type{ Ref } from 'vue';
+
+const props = defineProps<IuserInfo>()
+const name = ref(props.name)
+
+const change:Ref<string> = ref('')
+
+
+const  emitir =defineEmits(['Goodbye'])
+const clcikHandler = () => {
+    emitir('Goodbye')
+
+}
+
+
+const changev = computed(()=>change)
+const validationchange = computed(()=> !(change.value.length > 1))
+</script>
+
+
+<style scoped>
+.card {
+  background-color: blueviolet;
+  color: white;
+  width: 300px;
+  border: rgb(224, 0, 0) solid 2px;
+  border-radius: 10px;
+
+  padding: 8px 12px;
+}
+</style>
---
+++ src/data/UserData.ts
@@ -0,0 +1,41 @@
+import type { IuserInfo } from "@/interfaces/IUserInfo"
+
+const Users: IuserInfo[] = [
+  {
+    id: 1,
+    name: 'Ricardo',
+    lastName: 'Cruz',
+    email: 'elprofe@gmail.com',
+    age: 24
+  },
+  {
+    id: 2,
+    name: 'Ana',
+    lastName: 'Teresa',
+    email: 'Ana@gmail.com',
+    age: 28
+  },
+  {
+    id: 3,
+    name: 'Noemi',
+    lastName: 'De las nieves',
+    email: 'Noemi@gmail.com',
+    age: 28
+  },
+  {
+    id: 4,
+    name: 'Peter',
+    lastName: 'Parker',
+    email: 'hombrequearaña@gmail.com',
+    age: 28
+  },
+  {
+    id: 5,
+    name: 'Luffy',
+    lastName: 'D',
+    email: 'elastiman@gmail.com',
+    age: 28
+  },
+]
+
+export default Users
